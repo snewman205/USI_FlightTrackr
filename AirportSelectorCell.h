907294df5fc28038125dd1202a3d7f5599cceff0
Unbounded <unbounded@Apple-Users-MacBook.local>
2012-10-19 20:25:12 -0400

    Initial commit

---
+++ AirportSelectorCell.h
@@ -0,0 +1,16 @@
+//
+//  AirportSelectorCell.h
+//  FlightTrackr
+//
+//  Created by Unbounded on 9/29/12.
+//  Copyright (c) 2012 Scott Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirportSelectorCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UITextField *inpMenuItem;
+@property (strong, nonatomic) IBOutlet UILabel *lblMenuItemLabel;
+
+@end
---
+++ AirportSelectorCell.m
@@ -0,0 +1,29 @@
+//
+//  AirportSelectorCell.m
+//  FlightTrackr
+//
+//  Created by Unbounded on 9/29/12.
+//  Copyright (c) 2012 Scott Newman. All rights reserved.
+//
+
+#import "AirportSelectorCell.h"
+
+@implementation AirportSelectorCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
